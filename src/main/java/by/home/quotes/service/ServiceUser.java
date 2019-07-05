44e490aa4f752636fc3637a03350183e0d6c4f54
pentalgin <dmitrii.bragin90@gmail.com>
2019-07-05 23:18:04 +0300

    add FlyWay

--- src/main/java/by/home/quotes/service/ServiceUser.java
+++ src/main/java/by/home/quotes/service/ServiceUser.java
@@ -6,13 +6,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
+import java.util.Map;
 
 public interface ServiceUser extends UserDetailsService {
     User getUsername(String username);
-    void save(User user);
+
+    void save(User user, String username, Map<String, String> form);
+
     UserDetails loadUserByUsername(String username);
+
     List<User> getUserAll();
+
     boolean addUser(User user);
 
     boolean activateUser(String code);
+
+    void updateProfile(User user, String password, String email);
 }
--- src/main/java/by/home/quotes/service/ServiceUserImpl.java
+++ src/main/java/by/home/quotes/service/ServiceUserImpl.java
@@ -6,10 +6,10 @@ import by.home.quotes.repositories.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceUserImpl implements ServiceUser {
@@ -27,7 +27,19 @@ public class ServiceUserImpl implements ServiceUser {
     }
 
     @Override
-    public void save(User user) {
+    public void save(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for(String key: form.keySet()){
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
         userRepo.save(user);
     }
 
@@ -51,6 +63,12 @@ public class ServiceUserImpl implements ServiceUser {
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
 
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
         if(!user.getEmail().isEmpty()){
             String message = String.format(
                     "Hello, %s! \n" +
@@ -61,8 +79,6 @@ public class ServiceUserImpl implements ServiceUser {
 
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     @Override
@@ -77,6 +93,27 @@ public class ServiceUserImpl implements ServiceUser {
         userRepo.save(user);
 
         return true;
+    }
+
+    @Override
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+        boolean isEmailChange = (email != null && !email.equals(userEmail) ||
+                                (userEmail != null && !userEmail.equals(email)));
+        if(isEmailChange){
+            user.setEmail(email);
+            if(StringUtils.isEmpty(email)){
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if(StringUtils.isEmpty(password)){
+            user.setPassword(password);
+        }
+        userRepo.save(user);
+        if(isEmailChange){
+            sendMessage(user);
+        }
     }
 
 }
