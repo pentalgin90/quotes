fd20058ea80c637b8c51d959fedc4cdb11e5de63
pentalgin <dmitrii.bragin90@gmail.com>
2019-07-07 12:39:56 +0300

    Revert "add Encoder and validation bean"
    This reverts commit 632ce571

--- src/main/java/by/home/quotes/config/WebSecurityConfig.java
+++ src/main/java/by/home/quotes/config/WebSecurityConfig.java
@@ -2,16 +2,13 @@ package by.home.quotes.config;
 
 import by.home.quotes.service.ServiceUser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,15 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ServiceUser serviceUser;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return new BCryptPasswordEncoder(8);
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -46,6 +34,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(serviceUser)
-                .passwordEncoder(passwordEncoder);
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
--- src/main/java/by/home/quotes/controller/MainController.java
+++ src/main/java/by/home/quotes/controller/MainController.java
@@ -7,20 +7,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
@@ -50,44 +45,30 @@ public class MainController {
     @PostMapping("/")
     public String addQuotes(
             @AuthenticationPrincipal User user,
-            @Valid Quote quote,
-            BindingResult bindingResult,
-            Model model,
-            @RequestParam("file") MultipartFile file
+            @RequestParam String text,
+            @RequestParam String tag,
+            @RequestParam("file") MultipartFile file,
+            Map<String, Object> model
     ) throws IOException {
-        quote.setAuthor(user);
+        Quote quote = new Quote(text, tag, user);
 
-        if(bindingResult.hasErrors()){
+        if(file != null && !file.getOriginalFilename().isEmpty()){
+            File uploadDir = new File(uploadPath);
 
-            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
-            model.mergeAttributes(errorsMap);
-            model.addAttribute("quote", quote);
-
-        } else {
-
-            if(file != null && !file.getOriginalFilename().isEmpty()){
-                File uploadDir = new File(uploadPath);
-
-                if(!uploadDir.exists()){
-                    uploadDir.mkdir();
-                }
-
-                String uuidFile = UUID.randomUUID().toString();
-                String resultFileName = uuidFile + "." + file.getOriginalFilename();
-
-                file.transferTo(new File(uploadPath + "/" + resultFileName));
-
-                quote.setFilename(resultFileName);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
             }
 
-            model.addAttribute("quote", null);
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
 
-            serviceQuote.save(quote);
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
 
+            quote.setFilename(resultFileName);
         }
+        serviceQuote.save(quote);
         Iterable<Quote> quotes = serviceQuote.getAll();
-        model.addAttribute("quotes", quotes);
+        model.put("quotes", quotes);
         return "main";
     }
-
 }
