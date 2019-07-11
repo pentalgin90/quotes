4ae64127ac760c0396d4945ad326f53943488d84
pentalgin <dmitrii.bragin90@gmail.com>
2019-07-11 21:28:14 +0300

    del cycle dependencies

--- src/main/java/by/home/quotes/controller/MainController.java
+++ src/main/java/by/home/quotes/controller/MainController.java
@@ -3,12 +3,12 @@ package by.home.quotes.controller;
 import by.home.quotes.domain.Quote;
 import by.home.quotes.domain.User;
 import by.home.quotes.service.ServiceQuote;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,20 +19,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
 
     @Value("${upload.path}")
     private String uploadPath;
-
-    private final ServiceQuote serviceQuote;
-
-    public MainController(ServiceQuote serviceQuote) {
-        this.serviceQuote = serviceQuote;
-    }
+    @Autowired
+    private ServiceQuote serviceQuote;
 
     @GetMapping("/main")
     public String greeting() {
